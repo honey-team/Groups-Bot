7aa37c25579d250a6ccede8dc48b2777dd2bd111
TheHakerTech <aller_top@mail.ru>
2024-09-27 14:50:45 +0700

    Fixed name error

--- cogs/admin.py
+++ cogs/admin.py
@@ -2,9 +2,9 @@ import disnake, datetime
 from disnake.ext import commands
 from services.interfaces import AdminCommandsInterface
 
-class MemberCog(commands.Cog, AdminCommandsInterface):
+class AdminCog(commands.Cog, AdminCommandsInterface):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
 
 def setup(bot: commands.Bot):
-    bot.add_cog(MemberCog(bot))
+    bot.add_cog(AdminCog(bot))
